a7d794b1cf1256f6e0f3c95a190913fdf75dd3f1
Sodaman <mattrick2214@gmail.com>
2022-01-15 20:01:19 -0600

    wrote function and unique inquirer prompt idea.

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const employeeGenerator = require('./lib/Employee.js');
-const managerGenerator
-const engineerGenerator
-const internGenerator
+const inquirer = require('inquirer');
+const generateSite = require('./utils/generate-site.js');
+const fs = require('fs');
 
-new employeeGenerator()
-then.new managerGenerator()
-then.new engineerGenerator();
-then.new internGenerator();
-then .initalizeGenerator();
+const writeFile = fileContent => {
+    return new Promise ((resolve, reject) => {
+        fs.writeFile('.index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File Created!'
+            });
+        });
+    });
+};
+module.exports = { writeFile };
+
+const promptQuestions = () => {
+    return inquirer.prompt([Employee, Manager, Engineer, Intern])
+}
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+promptQuestions()
+.then(teamData => {
+    return generateSite(teamData);
+})
+.then(pageIndex => {
+    return writeFile(pageIndex);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse);
+})
+.catch(err => {
+    console.log(err)
+});
