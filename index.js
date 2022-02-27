35b3cfd761f76205491e2a4aa00b9ff5256193ad
Sodaman <mattrick2214@gmail.com>
2022-02-27 00:25:53 -0600

    moved copyFile and writeFile into another folder named utils for easier access.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const Employee = require('./lib/Employee');
-// const Manager = require('./lib/Manager');
-// const Engineer = require('./lib/Engineer');
-// const Intern = require('./lib/Intern');
-
-const employeeID = [];
-const teamMembers = [];
+const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 
 const promptManager = () => {
@@ -119,54 +113,20 @@ const promptEngineer = teamProfileData => {
     ])
 };
 
-const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-      fs.writeFile('./dist/index.html', fileContent, err => {
-        // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
-        if (err) {
-          reject(err);
-          // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
-          return;
-        }
-  
-        // if everything went well, resolve the Promise and send the successful data to the `.then()` method
-        resolve({
-          ok: true,
-          message: 'File created!'
-        });
-      });
-    });
-  };
-
-const copyFile = () => {
-    return new Promise((resolve, reject) => {
-        fs.copyFile('./src/style.css', './dist/style.css', err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-
-            resolve({
-                ok: true,
-                message: 'Stylesheet created!'
-            });
-        });
-    });
-};
-
 promptManager()
-.then(promptEngineer)
-.then(pageIndex => {
-    return writeFile(pageIndex);
-})
-
-.then(writeFileResponse => {
-    console.log(writeFileResponse);
-    return copyFile();
-})
-.then(copyFileResponse => {
-    console.log(copyFileResponse);
-})
-.catch(err => {
-    console.log(err)
-});
+    .then(teamData => {
+        return generatePage(teamData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
